5e3c9499d2c79b3cf1b06bb5b92a34ed19a388b4
prudhvipinnamani <53565469+prudhvipinnamani@users.noreply.github.com>
2021-05-10 20:28:10 +0530

    Add files via upload

---
+++ basic_java/Comments.java
@@ -0,0 +1,22 @@
+/** 
+* <h1> example class for all types of comments
+*class called "Comments" is created
+*comments are included in that class
+*/
+
+class Comments {
+	
+	
+	public static void main(String[] args) {
+	
+		//single line comments     
+		
+	
+		/*starting multiline comments
+         line 1
+          line 2		 
+          line 3
+		comment ends*/
+		
+	}
+}	
