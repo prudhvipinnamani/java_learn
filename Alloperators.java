782e5e3a852de4925924515fee10f0c5634ea5e7
prudhvipinnamani <53565469+prudhvipinnamani@users.noreply.github.com>
2021-05-10 20:26:28 +0530

    Update Alloperators.java

--- Alloperators.java
+++ Alloperators.java
@@ -1,12 +1,9 @@
 // Create a class with all operaters
 
 public class Alloperators {
-	
-	
-	
 	public static void main(String[] args) {
 	    int a=10;
-		int b=20;
+	    int b=20;
 	    int c=25;
 		int d=a+b-c;
 		int e=a*b%c;
